b1f9bf7935117165860f8350d329ae8241dc7c96
soorajshankar <soorajshankar@gmail.com>
2020-03-22 20:11:20 +0530

    Better Cron with await

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -8,11 +8,21 @@ import { UrlInput } from 'src/urls/inputs/url.input';
 export class CronService {
     constructor(private readonly urlsService: UrlsService) {}
     private readonly logger = new Logger(CronService.name);
+    private urls = []; // in memory list of active urls
 
-    @Cron('* * * * * *')
+    @Cron('10 * * * * *') // every minute 45 th second
+    async refetchActive() {
+        this.logger.debug('>> refetching url');
+        this.urls = await this.urlsService.findAll();
+    }
+
+    @Cron('* * * * * *') // testing with every second
     async handleCron() {
-        const res = await this.urlsService.findAll();
-        this.logger.debug('>>' + (await this.getUrl('http://google.com')));
+        this.urls.forEach(async i => {
+            this.logger.debug('URL>', i.url);
+            const rr = await axios.get(i.url);
+            this.logger.debug('URL?>', rr.status + '');
+        });
     }
     async getUrl(url: string) {
         return await axios.get(url);
