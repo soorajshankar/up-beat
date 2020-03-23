de33b7a9cbd527a4375cfea3bf47fc89e5c0aebb
soorajshankar <soorajshankar@gmail.com>
2020-03-23 21:50:46 +0530

    mutations and query schema

--- src/analytics/analytics.resolver.ts
+++ src/analytics/analytics.resolver.ts
@@ -7,18 +7,13 @@ import { AnalyticsInput } from './inputs/analytics.input';
 @Resolver('Urls')
 export class AnalyticsResolver {
     constructor(private readonly analyticsService: AnalyticsService) {}
-    @Query(() => String)
-    async hello() {
-        return 'hello';
-    }
-
     @Query(() => [AnalyticsType])
-    async urls() {
+    async analytics() {
         return this.analyticsService.findAll();
     }
 
     @Mutation(() => AnalyticsType)
-    async createUrl(@Args('input') input: AnalyticsInput) {
+    async createAnalytics(@Args('input') input: AnalyticsInput) {
         return this.analyticsService.create(input);
     }
 }
