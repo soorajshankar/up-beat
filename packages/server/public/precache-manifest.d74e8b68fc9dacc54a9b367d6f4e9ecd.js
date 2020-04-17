7660e77b247dce34aae878ff191ac063e2d09336
soorajshankar <soorajshankar@gmail.com>
2020-04-17 17:45:46 +0530

    ui build added to repo

---
+++ packages/server/public/precache-manifest.d74e8b68fc9dacc54a9b367d6f4e9ecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a5b2b0561676e12f73b8c101bd786f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbc9135c135582626b00",
+    "url": "/static/css/2.3abc28ca.chunk.css"
+  },
+  {
+    "revision": "d6b911f1d2999be0b444",
+    "url": "/static/css/main.36cfc3d2.chunk.css"
+  },
+  {
+    "revision": "bbc9135c135582626b00",
+    "url": "/static/js/2.ba951310.chunk.js"
+  },
+  {
+    "revision": "740359214ef94c69fe053ceb4a9f0ac1",
+    "url": "/static/js/2.ba951310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b911f1d2999be0b444",
+    "url": "/static/js/main.634f0b5f.chunk.js"
+  },
+  {
+    "revision": "fd5c047c84371646bb95",
+    "url": "/static/js/runtime-main.251e27e0.js"
+  }
+]);
